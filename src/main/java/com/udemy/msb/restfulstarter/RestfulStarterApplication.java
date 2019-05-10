8ca27123bc95905cc5b84cedc44804f81499d2e8
SergeA <ronnieo@gmail.com>
2019-05-09 21:48:16 -0400

    Initial commit

---
+++ src/main/java/com/udemy/msb/restfulstarter/RestfulStarterApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.msb.restfulstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/msb/restfulstarter/RestfulStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udemy.msb.restfulstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulStarterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
