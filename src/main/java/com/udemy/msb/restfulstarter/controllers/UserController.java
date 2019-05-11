84e83cdb0d1d2842103a4540a9de166b5949f01c
SergeA <ronnieo@gmail.com>
2019-05-10 20:55:56 -0400

    Added UserController

---
+++ src/main/java/com/udemy/msb/restfulstarter/controllers/UserController.java
@@ -0,0 +1,28 @@
+package com.udemy.msb.restfulstarter.controllers;
+
+import com.udemy.msb.restfulstarter.domain.User;
+import com.udemy.msb.restfulstarter.services.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+}
