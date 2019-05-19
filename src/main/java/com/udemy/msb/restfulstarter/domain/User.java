821d364205047f6e1e8ef008f16433d2510cc98b
SergeA <ronnieo@gmail.com>
2019-05-19 13:10:41 -0400

    Added swagger configuration

--- src/main/java/com/udemy/msb/restfulstarter/domain/User.java
+++ src/main/java/com/udemy/msb/restfulstarter/domain/User.java
@@ -1,21 +1,29 @@
 package com.udemy.msb.restfulstarter.domain;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@ApiModel("User Details")
 public class User {
     private Long id;
 
     @NotBlank
+    @Size(min = 2, message = "First Name should be at least 2 characters")
+    @ApiModelProperty("First Name: at least 2 characters")
     private String firstName;
 
     @NotBlank
     private String lastName;
 
     @Past
+    @ApiModelProperty("Birth Date: should be in the past")
     private Date birthDate;
 
     private List<Post> posts = new ArrayList<>();
