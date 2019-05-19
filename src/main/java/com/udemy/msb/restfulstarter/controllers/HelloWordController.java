2894e56cebe5becdd1ff0476da3882272f43c666
SergeA <ronnieo@gmail.com>
2019-05-19 12:28:16 -0400

    Added i18n sample

--- src/main/java/com/udemy/msb/restfulstarter/controllers/HelloWordController.java
+++ src/main/java/com/udemy/msb/restfulstarter/controllers/HelloWordController.java
@@ -1,11 +1,19 @@
 package com.udemy.msb.restfulstarter.controllers;
 import com.udemy.msb.restfulstarter.domain.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWordController {
+
+    private MessageSource messageSource;
+
+    public HelloWordController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -20,5 +28,11 @@ public class HelloWordController {
     @GetMapping("/hello-world-bean/{name}")
     public HelloWorldBean helloWorldBeanPath(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        return messageSource.getMessage("good.morning.message", null,
+                LocaleContextHolder.getLocale());
     }
 }
